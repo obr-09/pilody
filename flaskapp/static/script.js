f6e557d758932ce200f8a0ef5d9291637cc268aa
Zessirb <zessirb@gmail.com>
2018-09-01 23:45:26 +0200

    Added AJAX methods to go next, previous and to pause/resume the music

--- flaskapp/static/script.js
+++ flaskapp/static/script.js
@@ -1,1 +1,21 @@
-console.log(document.getElementsByName('body')[0].getAttribute('data-url'));
+var gui_url = document.getElementsByTagName('body')[0].getAttribute('data-url');
+var base_url = gui_url.replace(/\/?gui\/?/, '');
+
+
+function play() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', base_url + '/control', true);
+    xhttp.send('action=pause');
+}
+
+function previous() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', base_url + '/control', true);
+    xhttp.send('action=previous');
+}
+
+function next() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', base_url + '/control', true);
+    xhttp.send('action=next');
+}
