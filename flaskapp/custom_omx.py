53cb38c47e9e0aa21f26b2d99cfc59798554c68d
Zessirb <zessirb@gmail.com>
2018-09-01 18:19:15 +0200

    Added previous command

--- flaskapp/custom_omx.py
+++ flaskapp/custom_omx.py
@@ -1,4 +1,4 @@
-from queue import Queue, Empty
+from queue import Queue, Empty, LifoQueue
 from threading import Event, Thread
 from time import sleep
 
@@ -9,11 +9,11 @@ class CustomOMX:
 
     def __init__(self):
         self.music_queue = Queue()
-        self.previous_queue = Queue()
+        self.previous_queue = LifoQueue()
         self.pause_event = Event()
         self.stop_event = Event()
         self.player_thread = Thread(target=CustomOMX.run_music,
-                                    args=(self.music_queue, self.previous.queue, self.pause_event, self.stop_event))
+                                    args=(self.music_queue, self.previous_queue, self.pause_event, self.stop_event))
         self.player_thread.start()
 
     def set_audio(self, url):
@@ -38,17 +38,24 @@ class CustomOMX:
         self.pause_event.set()
 
     def previous(self):
-        pass
+        previous_music = self.previous_queue.get(False)
+        if previous_music:
+            next_musics = self.empty_queue()
+            next_musics.insert(0, previous_music)
+            self.set_playlist(next_musics)
 
     def next(self):
         self.stop_event.set()
 
     def empty_queue(self):
+        queue_content = []
         try:
-            while self.music_queue.get(False):
-                pass
+            queue_element = self.music_queue.get(False)
+            while queue_element:
+                queue_content.append(queue_element)
         except Empty:
             pass
+        return queue_content
 
     @staticmethod
     def run_music(music_queue, previous_queue, pause_event, stop_event):
@@ -66,4 +73,4 @@ class CustomOMX:
                     player.quit()
                     player = None
                     stop_event.clear()
-
+            previous_queue.put(music)
