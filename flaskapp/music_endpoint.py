a559ced57ae4b574c5923677e1e8bfae16e8963a
Zessirb <zessirb@gmail.com>
2018-09-01 18:59:07 +0200

    Fixed wrong import

--- flaskapp/music_endpoint.py
+++ flaskapp/music_endpoint.py
@@ -1,6 +1,6 @@
 from flask import current_app, request
 from flask_restful import Resource
-from flask_restful_swagger_2 import swagger
+from flask_restful_swagger import swagger
 
 from flaskapp.youtube_utility import YoutubeUtility
 
