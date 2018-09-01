65766468ff41d7c434b19420cb8e3bbd5d4f7cae
Zessirb <zessirb@gmail.com>
2018-09-01 23:53:47 +0200

    Added content type request header

--- flaskapp/static/script.js
+++ flaskapp/static/script.js
@@ -5,17 +5,20 @@ var base_url = gui_url.replace(/\/?gui\/?/, '');
 function play() {
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', base_url + '/control', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send('action=pause');
 }
 
 function previous() {
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', base_url + '/control', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send('action=previous');
 }
 
 function next() {
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', base_url + '/control', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send('action=next');
 }
