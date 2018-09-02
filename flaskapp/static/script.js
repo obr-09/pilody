64932cd6a5535e280b192c08436ba469464d6e79
zessirb <zessirb@gmail.com>
2018-09-02 21:38:37 +0200

    Centered GUI and added color feedback for buttons

--- flaskapp/static/script.js
+++ flaskapp/static/script.js
@@ -2,23 +2,64 @@ var gui_url = document.getElementsByTagName('body')[0].getAttribute('data-url');
 var base_url = gui_url.replace(/\/?gui\/?/, '');
 
 
+var playTriggered = false;
 function play() {
+    if (playTriggered) {
+        return;
+    }
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', base_url + '/control', true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function() {
+        var buttonPlay = document.getElementById("button-play");
+        playTriggered = true;
+        buttonPlay.classList.add((xhttp.status % 100 === 2) ? "button-success" : "button-error");
+        setTimeout(function() {
+            buttonPlay.classList.remove("button-success", "button-error");
+            playTriggered = false;
+        }, 1000);
+    };
     xhttp.send('action=pause');
 }
 
+
+var previousTriggered = false;
 function previous() {
+    if (previousTriggered) {
+        return;
+    }
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', base_url + '/control', true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function() {
+        var buttonPrevious = document.getElementById("button-previous");
+        previousTriggered = true;
+        buttonPrevious.classList.add((xhttp.status % 100 === 2) ? "button-success" : "button-error");
+        setTimeout(function() {
+            buttonPrevious.classList.remove("button-success", "button-error");
+            previousTriggered = false;
+        }, 1000);
+    };
     xhttp.send('action=previous');
 }
 
+
+var nextTriggered = false;
 function next() {
+    if (nextTriggered) {
+        return;
+    }
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', base_url + '/control', true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function() {
+        var buttonNext = document.getElementById("button-next");
+        nextTriggered = true;
+        buttonNext.classList.add((xhttp.status % 100 === 2) ? "button-success" : "button-error");
+        setTimeout(function() {
+            buttonNext.classList.remove("button-success", "button-error");
+            nextTriggered = false;
+        }, 1000);
+    };
     xhttp.send('action=next');
 }
