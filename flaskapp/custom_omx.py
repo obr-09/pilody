ec79c39d8ba11ed2855fd44bd700c1eb01cd50fa
Zessirb <zessirb@gmail.com>
2018-09-02 18:29:30 +0200

    Forgot to clear previous_event

--- flaskapp/custom_omx.py
+++ flaskapp/custom_omx.py
@@ -35,9 +35,7 @@ class CustomOMX:
     def set_playlist(self, url_list):
         self.empty_queue()
         self.stop_event.set()
-        print('set_playlist')
         for url in url_list:
-            print(url)
             self.next_queue.put(url)
         self.pause_event.set()
 
@@ -63,11 +61,9 @@ class CustomOMX:
 
     def empty_queue(self):
         queue_content = []
-        print('empty_queue')
         try:
             queue_element = self.next_queue.get(False)
             while queue_element:
-                print(queue_element)
                 queue_content.append(queue_element)
                 queue_element = self.next_queue.get(False)
         except Empty:
@@ -118,14 +114,10 @@ class OMXRunner:
         self.stop_event.clear()
         self.stop()
         musics_queued = [self.current_music] if self.current_music else []
-        print('previous')
-        if self.current_music:
-            print(self.current_music)
         try:
             self.current_music = self.next_queue.get_nowait()
             music_queued = self.next_queue.get_nowait()
             while music_queued:
-                print(music_queued)
                 musics_queued.append(music_queued)
                 music_queued = self.next_queue.get_nowait()
         except Empty:
@@ -134,6 +126,7 @@ class OMXRunner:
             self.next_queue.put(music)
         if self.current_music:
             self.player = OMXPlayer(self.current_music)
+        self.previous_event.clear()
 
     def next(self):
         self.stop_event.clear()
