523f80205120b48da715c308164ca8caed71c4dd
Olivier Brissez <OBrissez@voyages-sncf.com>
2018-10-26 18:47:39 +0200

    Coding "addResource" to add a video from input

--- flaskapp/static/add.js
+++ flaskapp/static/add.js
@@ -9,18 +9,22 @@ $('#keyboard').jkeyboard({
 
 
 function addResource() {
-    var uriRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
     var search = $('#searchField').val();
-    var isUri = search.match(uriRegex);
-    if (isUri !== null) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open('POST', base_url + '/playlist', true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send('playlist_url=' + encodeURI(search));
-        xhttp.onreadystatechange = function() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', base_url + '/music', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send('youtube_url=' + encodeURI(search));
+    xhttp.onreadystatechange = function() {
+        if (xhttp.status >= 400) {
+            xhttp = new XMLHttpRequest();
+            xhttp.open('POST', base_url + '/playlist', true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send('playlist_url=' + encodeURI(search));
+            xhttp.onreadystatechange = function() {
+                $('#searchField').val('');
+            };
+        } else {
             $('#searchField').val('');
-        };
-    } else {
-
-    }
+        }
+    };
 }
