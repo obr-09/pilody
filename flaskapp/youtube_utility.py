891053f4f9e9a85ba180250921752f848b392a07
zessirb <zessirb@gmail.com>
2019-01-03 20:32:53 +0100

    Added method "get_youtube_next_video", using an url as parameter

--- flaskapp/youtube_utility.py
+++ flaskapp/youtube_utility.py
@@ -1,4 +1,6 @@
+from bs4 import BeautifulSoup
 import pafy
+import urllib
 
 
 class YoutubeUtility:
@@ -16,3 +18,15 @@ class YoutubeUtility:
         for video in video_list:
             video.audio_url = video.getbestaudio().url
         return video_list
+
+    @staticmethod
+    def get_youtube_next_video(youtube_url):
+        try:
+            soup = BeautifulSoup(urllib.request.urlopen(youtube_url))
+            next_video_item = soup.find('li', {'class': 'video-list-item'})
+            if next_video_item:
+                next_video_link = next_video_item.find('a')
+                if next_video_link:
+                    return 'https://www.youtube.com' + next_video_link.get('href')
+        except ValueError:
+            pass
